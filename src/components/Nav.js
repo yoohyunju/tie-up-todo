ede5475a636e83cd9eec45c4c0be034bb9bb2959
hazel <yhj2756@gmail.com>
2022-07-07 00:55:02 +0900

    initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div id="nav-body">
+      <span id="title">
+        <img id="logo" src="" alt="logo" />
+        <span id="name">tie-up</span>
+      </span>
+      <div id="menu">
+        <span>메뉴바</span>
+      </div>
+    </div>
+  )
+}
+
+export default Nav;
